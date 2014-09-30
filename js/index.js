62bb159bdbd08c7180616eecabadb7fdddd89d8a
blackie1019 <blackie1019@gmail.comm>
2014-10-01 03:27:13 +0800

    add onbeforeunload and unload event to prompt out Stay msg.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,39 @@
-window.onunload = window.onbeforeunload = (function() {
-    closeWindow()
-});
-$('#closeWindowModal').modal('show');
-$(function() {
-    function closeWindow() {
-        $('#closeWindowModal').modal('show');
+var wpoto_timeout,
+    allowRedirect = false,
+    preventExitPop = false;
+
+window.onbeforeunload = function warning() {
+    if (preventExitPop == false) {
+        if (!allowRedirect) {
+            wpoto_timeout = setTimeout(function() {
+                showExitPop();
+            }, 1);
+            allowRedirect = true;
+            return false;
+        }
     }
+};
+window.unload = function noTimeout() {
+    clearTimeout(wpoto_timeout);
+    preventExitPop = true;
+}
+
+function showExitPop() {
+    $('#closeWindowModal').modal('show');
+}
+
+function closeWindow() {
+    
+}
+
+$(function() {
+
+    $('#btnModalStayConfirm').on('click', function() {
+        $('#content2').removeClass('hide');
+        $('#content1').addClass('hide');;
+    });
+    $('#btnModalExitConfirm').on('click', function() {
+        closeWindow();
+    });
+
 });
